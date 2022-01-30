aaa17e79180188f245db5777965f283b8cdcc640
Rodrigo Araujo <rodrigoaraujo@cers.com.br>
2022-01-30 16:42:01 -0300

    backgrund change

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function HomePage() {
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
                     backgroundColor: appConfig.theme.colors.primary[500],
-                    backgroundImage: 'url(https://i.giphy.com/media/AOSwwqVjNZlDO/giphy.webp)',
+                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
             >
